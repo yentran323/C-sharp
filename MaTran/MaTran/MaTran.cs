1114a5066d7741d1a9e4fc5afd5fe09d5b4a12fd
Yến Trần <yentth.2001@gmail.com>
2025-02-08 16:12:01 +0700

    Matrices exercise

---
+++ MaTran/MaTran/MaTran.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaTran
+{
+    class MaTran
+    {
+        private int m; //So dong cua ma tran
+        private int n; //So cot cua ma tran
+        int[,] matran;
+
+        public MaTran(int m, int n)
+        {
+            this.n = n;
+            this.m = m;
+            matran = new int[m, n];
+        }
+        public MaTran(int[,] maTran)
+        {
+            matran = new int[maTran.GetLength(0), maTran.GetLength(1)];
+            for (int i = 0; i < maTran.GetLength(0); i++)
+            {
+                for (int j = 0; j < maTran.GetLength(1); j++)
+                {
+                    matran[i, j] = maTran[i, j];
+                }
+            }
+        }
+
+        public void NhapMaTran()
+        {
+            for (int i = 0; i < matran.GetLength(0); i++)
+            {
+                for (int j = 0; j < matran.GetLength(1); j++)
+                {
+                    Console.Write("Nhap phan tu [{0},{1}]: ", i, j);
+                    matran[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+        }
+        public void InMaTran()
+        {
+            for (int i = 0; i < matran.GetLength(0); i++)
+            {
+                for (int j = 0; j < matran.GetLength(1); j++)
+                {
+                    Console.Write("{0,10}", matran[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        protected int[,] ConvertToMang2Chieu()
+        {
+            int[,] cv = new int[m, n];
+            for (int i = 0; i < matran.GetLength(0); i++)
+            {
+                for (int j = 0; j < matran.GetLength(1); j++)
+                {
+                    cv[i, j] = matran[i, j];
+                }
+            }
+            return cv;
+        }
+        public MaTran Tong(MaTran a, MaTran b)
+        {
+            int [,] mt1 = a.ConvertToMang2Chieu();
+            int[,] mt2 = b.ConvertToMang2Chieu();
+            int[,] ketqua = new int[a.m, a.n];
+
+            for (int i = 0; i < a.m; i++)
+            {
+                for (int j = 0; j < a.n; j++)
+                {
+                    ketqua[i, j] = mt1[i, j] + mt2[i, j];
+                }
+            }
+            MaTran kq = new MaTran(ketqua);
+            return kq;
+        }
+        public MaTran Hieu(MaTran a, MaTran b)
+        {
+            int[,] mt1 = a.ConvertToMang2Chieu();
+            int[,] mt2 = b.ConvertToMang2Chieu();
+            int[,] ketqua = new int[a.m, a.n];
+
+            for (int i = 0; i < a.m; i++)
+            {
+                for (int j = 0; j < a.n; j++)
+                {
+                    ketqua[i, j] = mt1[i, j] + (-1)*mt2[i, j];
+                }
+            }
+            MaTran kq = new MaTran(ketqua);
+            return kq;
+        }
+
+        public MaTran Tich(MaTran a, MaTran b)
+        {
+            int[,] mt1 = a.ConvertToMang2Chieu();
+            int[,] mt2 = b.ConvertToMang2Chieu();
+            int[,] ketqua = new int[a.m, b.n];
+
+            for (int i = 0; i < a.m; i++) //hang cua ma tran a
+            {
+                for (int j = 0; j < b.n; j++) // cot cua ma tran b
+                {
+                    int tong = 0;
+                    for (int k = 0; k < a.n; k++) 
+                    {
+                        tong += mt1[i, k] * mt2[k, j];
+                    }
+                    ketqua[i, j] = tong;
+                }
+            }
+            MaTran kq = new MaTran(ketqua);
+            return kq;
+        }
+        public MaTran MaTranChuyenVi()
+        {
+            int[,] cv = new int[n, m];
+            for (int i = 0; i < matran.GetLength(0); i++)
+            {
+                for (int j = 0; j < matran.GetLength(1); j++)
+                {
+                    cv[j, i] = matran[i, j];
+                }
+            }
+            MaTran kq = new MaTran(cv);
+            return kq;
+        }
+        public bool KiemTraMaTranVuong()
+        {
+            bool kt = false;
+            if (m == n) kt = true;
+            return kt;
+        }
+    }
+}
---
+++ MaTran/MaTran/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace MaTran
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap so dong cua ma tran thu nhat:");
+            int m1 = Int32.Parse(Console.ReadLine());
+            Console.Write("Nhap so cot cua ma tran thu nhat:");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            MaTran a = new MaTran(m1, n1);
+            Console.WriteLine("Nhap ma tran a:");
+            a.NhapMaTran();
+            Console.WriteLine("Ma tran a:");
+            a.InMaTran();
+
+            Console.Write("Nhap so dong cua ma tran thu hai:");
+            int m2 = Int32.Parse(Console.ReadLine());
+            Console.Write("Nhap so cot cua ma tran thu hai:");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            MaTran b = new MaTran(m2, n2);
+            Console.WriteLine("Nhap ma tran b:");
+            b.NhapMaTran();
+            Console.WriteLine("Ma tran b:");
+            b.InMaTran();
+
+            MaTran c = new MaTran(m1, n2);
+            if (m1 != m2 || n1 != n2)
+                Console.WriteLine("Khong the cong 2 ma tran khong cung co");
+            else
+            {
+                Console.WriteLine("Tong hai ma tran:");
+                c = c.Tong(a, b);
+                c.InMaTran();
+            }
+
+            if (m1 != m2 || n1 != n2)
+                Console.WriteLine("Khong the tinh hieu 2 ma tran khong cung co");
+            else
+            {
+                Console.WriteLine("Hieu hai ma tran:");
+                c = c.Hieu(a, b);
+                c.InMaTran();
+            }
+
+            if (n1 != m2)
+                Console.WriteLine("Khong the nhan hai ma tran");
+            else
+            {
+                Console.WriteLine("Tich hai ma tran:");
+                c = c.Tich(a, b);
+                c.InMaTran();
+            }
+
+            Console.WriteLine("Ma tran chuyen vi cua a la:");
+            MaTran cv = a.MaTranChuyenVi();
+            cv.InMaTran();
+
+            if (a.KiemTraMaTranVuong())
+                Console.WriteLine("Ma tran a la ma tran vuong");
+            else
+                Console.WriteLine("Ma tran a khong la ma tran vuong");
+
+            Console.ReadLine();
+        }
+    }
+}
